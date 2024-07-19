024ee8f073f87e3d95301f54643feecc7df73475
DDeviak <117632082+DDeviak@users.noreply.github.com>
2024-07-19 20:01:30 +0300

    chore: Update model properties to use non-nullable types

--- ToDoListApp.DAL/Models/User.cs
+++ ToDoListApp.DAL/Models/User.cs
@@ -3,9 +3,9 @@ namespace ToDoListApp.DAL.Models
     public class User
     {
         public Guid Id { get; set; }
-        public string? Username { get; set; }
-        public string? Email { get; set; }
-        public string? PasswordHash { get; set; }
-        public List<Tasklist>? Tasklists { get; set; }
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public List<Tasklist> Tasklists { get; set; } = null!;
     }
 }
