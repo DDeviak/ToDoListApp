2d7e39c1bbda7a29f3ea57579d2df4d7b0edaca4
DDeviak <117632082+DDeviak@users.noreply.github.com>
2024-07-21 23:06:56 +0300

    chore: Update UserProfile mapping to use UserRegisterDTO instead of UserCreateDTO

--- ToDoListApp.BLL/Mapping/UserProfile.cs
+++ ToDoListApp.BLL/Mapping/UserProfile.cs
@@ -7,7 +7,7 @@ public class UserProfile : Profile
 {
     public UserProfile()
     {
-        CreateMap<User, UserCreateDTO>().ReverseMap();
+        CreateMap<User, UserRegisterDTO>().ReverseMap();
         CreateMap<User, UserDTO>().ReverseMap();
     }
 }
