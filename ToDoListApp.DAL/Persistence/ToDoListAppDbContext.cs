2fb9b3d6b81f37f211c629b06ade78b2fe265707
DDeviak <117632082+DDeviak@users.noreply.github.com>
2024-07-21 23:08:59 +0300

    chore: Update DbContext

--- ToDoListApp.DAL/Persistence/ToDoListAppDbContext.cs
+++ ToDoListApp.DAL/Persistence/ToDoListAppDbContext.cs
@@ -1,35 +1,38 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using ToDoListApp.DAL.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ToDoListApp.DAL.Models;
 
 namespace ToDoListApp.DAL.Persistence
 {
     public class ToDoListAppDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
     {
+        public ToDoListAppDbContext(DbContextOptions<ToDoListAppDbContext> options)
+            : base(options)
+        {
+        }
+
         public override DbSet<User> Users { get; set; }
 
         public DbSet<Tasklist> Tasklists { get; set; }
 
         public DbSet<TaskToDo> Tasks { get; set; }
 
-        public ToDoListAppDbContext(DbContextOptions<ToDoListAppDbContext> options) : base(options)
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-        }
+            base.OnModelCreating(builder);
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<User>()
+            builder.Entity<User>()
                 .HasMany(u => u.Tasklists)
                 .WithOne(tl => tl.User)
                 .HasForeignKey(tl => tl.UserId);
 
-            modelBuilder.Entity<Tasklist>()
+            builder.Entity<Tasklist>()
                 .HasMany(tl => tl.Tasks)
                 .WithOne(t => t.Tasklist)
                 .HasForeignKey(t => t.TasklistId);
 
-            modelBuilder.Entity<TaskToDo>()
+            builder.Entity<TaskToDo>()
                 .Property(t => t.Status)
                 .HasConversion<string>();
         }
